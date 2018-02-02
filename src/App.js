a74282d5dc598ad5f198b38eee77de14aa130abc
Paul <pfreema1@gmail.com>
2018-02-02 14:31:20 -0500

    fin

--- src/App.js
+++ src/App.js
@@ -1,66 +1,50 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import BlackBoxAnimated from "./BlackBoxAnimated";
+import { Motion, StaggeredMotion, spring } from "react-motion";
 
-const Wrapper = styled.div``;
+const colorArray = ["#DA3D52", "#F4DC71", "#F6FFFD", "#061934", "#485360"];
 
-const ImageBox = styled.div`
-  background: url("https://www.adventure-journal.com/wp-content/uploads/2015/06/o-the-glory-of-it-all.jpg");
-  width: 600px;
-  height: 400px;
-  background-size: cover;
-  background-position: center;
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  min-height: 100vh;
+  background: skyblue;
 `;
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const Box = styled.div`
+  flex-basis: ${props => props.width}%;
+  background: ${props => props.bgColor};
+  // border-right: 1px solid white;
+`;
 
-    this.state = {
-      animationNumber: 1
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(this.startNextAnimation, 500);
-    setTimeout(this.startNextAnimation, 1000);
-    setTimeout(this.startNextAnimation, 1500);
-  }
-
-  startNextAnimation = () => {
-    this.setState({
-      animationNumber: this.state.animationNumber + 1
-    });
-  };
-
+class EntranceAnimation extends Component {
   render() {
-    const { animationNumber } = this.state;
-
     return (
-      <Wrapper>
-        <ImageBox>
-          <BlackBoxAnimated
-            heightPercentage={25}
-            startAnimation={animationNumber >= 1}
-          />
-          <BlackBoxAnimated
-            heightPercentage={25}
-            reverseDirection
-            startAnimation={animationNumber >= 2}
-          />
-          <BlackBoxAnimated
-            heightPercentage={25}
-            startAnimation={animationNumber >= 3}
-          />
-          <BlackBoxAnimated
-            heightPercentage={25}
-            reverseDirection
-            startAnimation={animationNumber >= 4}
-          />
-        </ImageBox>
-      </Wrapper>
+      <StaggeredMotion
+        defaultStyles={[
+          { width: 100 },
+          { width: 100 },
+          { width: 100 },
+          { width: 100 }
+        ]}
+        styles={prevStyles => [
+          { width: spring(0) },
+          { width: spring(prevStyles[0].width) },
+          { width: spring(prevStyles[1].width) },
+          { width: spring(prevStyles[2].width) }
+        ]}
+      >
+        {styles => (
+          <Wrapper>
+            <Box bgColor={colorArray[0]} width={styles[0].width} />
+            <Box bgColor={colorArray[1]} width={styles[1].width} />
+            <Box bgColor={colorArray[3]} width={styles[2].width} />
+            <Box bgColor={colorArray[4]} width={styles[3].width} />
+          </Wrapper>
+        )}
+      </StaggeredMotion>
     );
   }
 }
 
-export default App;
+export default EntranceAnimation;
